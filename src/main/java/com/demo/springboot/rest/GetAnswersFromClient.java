f3899025c52191d878cc8d8c2b13b2916887b272
Marshall <kamil.budzik98@gmail.com>
2020-02-03 16:35:14 +0100

    wyswietla correct answers do pytania od klienta

--- src/main/java/com/demo/springboot/rest/GetAnswersFromClient.java
+++ src/main/java/com/demo/springboot/rest/GetAnswersFromClient.java
@@ -1,11 +1,16 @@
 package com.demo.springboot.rest;
 
 import com.demo.springboot.domain.dto.AnswerDto;
+import com.demo.springboot.domain.dto.Questions;
+import com.demo.springboot.domain.dto.ReturnQuestion;
+import com.demo.springboot.service.impl.QuizCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class GetAnswersFromClient {
@@ -13,9 +18,15 @@ public class GetAnswersFromClient {
 
     @PutMapping(value = "/quiz/calculate")
     public ResponseEntity<AnswerDto> test2(@RequestBody AnswerDto answerDto) {
+        System.out.print("Dobre odpowiedzi to pytania o id "+answerDto.getQuestionId()+" to: ");
+        ArrayList<Questions> quiz = new ArrayList<Questions>(QuizCode.readData());//pobiera correct answers do aktualnego id pytania (id pobrane od klienta)
+        System.out.println(quiz.get(answerDto.getQuestionId()).getCorrectAnswers());//pobiera correct answers do aktualnego id pytania (id pobrane od klienta)
+
+
         System.out.print("dziala klasa GetAnswersFromClient: ");
         LOGGER.info(answerDto.toString());
         return new ResponseEntity<AnswerDto>(answerDto, HttpStatus.OK);
+
     }
 
     public class CheckAnswer { //sprawdzanie odpowiedzi
