8a14cbe54209fbca6251d59760ff8a7c3df17a41
Kamil Pinas <kamilpinas@gmail.com>
2020-02-03 12:45:10 +0100

    // quiz/calculate

--- src/main/java/com/demo/springboot/rest/QuestionGet.java
+++ src/main/java/com/demo/springboot/rest/QuestionGet.java
@@ -1,30 +1,22 @@
 package com.demo.springboot.rest;
 
 import java.util.ArrayList;
+
+import com.demo.springboot.domain.dto.AnswerDto;
 import com.demo.springboot.domain.dto.Questions;
 import com.demo.springboot.domain.dto.ReturnQuestion;
 import com.demo.springboot.service.impl.QuizCode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-//@RequestMapping(value = "/api")
-
 public class QuestionGet {
     private static final Logger LOGGER = LoggerFactory.getLogger(QuestionGet.class);
-
-
     @RequestMapping(value = "/quiz/question/{id}", method = RequestMethod.GET)
-    public ResponseEntity<ReturnQuestion> test(@PathVariable("id") Integer id) {
-        id = id - 1;// zeby sie zgadzaly indexy XDDD
-
+    public ResponseEntity<ReturnQuestion>test(@PathVariable("id") Integer id) {
         boolean ifLast;
         if (id==9){
             ifLast=true;
@@ -32,7 +24,6 @@ public class QuestionGet {
         else{
             ifLast=false;
         }
-
         ArrayList<Questions> quiz = new ArrayList<Questions>(QuizCode.readData());
         final ReturnQuestion quizValues = new ReturnQuestion(
                 quiz.get(id).getQuestion(),
@@ -42,10 +33,14 @@ public class QuestionGet {
                 quiz.get(id).getAnswerD(),
                 quiz.get(id).getPoints(),
         ifLast);
-
-
         return new ResponseEntity<ReturnQuestion>(quizValues, HttpStatus.OK);
     }
+    @PutMapping(value = "/quiz/calculate")
+    public ResponseEntity<AnswerDto>test2(@RequestBody AnswerDto answerDto) {
+        LOGGER.info(answerDto.getSelectedAnswers());
+        return new ResponseEntity<AnswerDto>(answerDto, HttpStatus.OK);
+    }
+
 }
 
 
