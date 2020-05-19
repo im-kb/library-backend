7824e1fa3031a4e0e1ce239ae6183bedd483a681
Marshall <kamil.budzik98@gmail.com>
2020-05-19 16:07:45 +0200

    automatyzacja zapytan select

--- src/main/java/com/demo/springboot/rest/DBController.java
+++ src/main/java/com/demo/springboot/rest/DBController.java
@@ -11,24 +11,52 @@ public class DBController {
         String url = "jdbc:postgresql://rogue.db.elephantsql.com:5432/cargbzfv";
         String user = "cargbzfv";
         String password = "wjhaujNlFknUAm2GQ3jz6HOh9UNLYRB8";
-        System.out.println("\n \n \nDZIALA SQLLL ");
+        System.out.println("DZIALA SQLLL\n");
 
+        //////////////////////////ZAPYTANIE O WYDAWNICTWO
+        System.out.println("Wydawnictwo::::::::::::::::::::::::");
         try (Connection con = DriverManager.getConnection(url, user, password);
              PreparedStatement pst = con.prepareStatement("SELECT * FROM wydawnictwo");
              ResultSet rs = pst.executeQuery()) {
-            while (rs.next() != false) {
-                System.out.print(rs.getInt(1));
-                System.out.print(": ");
-                System.out.print(rs.getString(2));
-                System.out.print(": ");
-                System.out.print(rs.getString(3));
-                System.out.print("\n");
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
+                }
+                System.out.println("");
             }
-
         } catch (SQLException ex) {
-
             Logger lgr = Logger.getLogger(DBController.class.getName());
             lgr.log(Level.SEVERE, ex.getMessage(), ex);
         }
+        System.out.print("-------------------------------------\n\n");
+        //////////////////////////ZAPYTANIE O WYDAWNICTWO
+
+        //////////////////////////ZAPYTANIE O KLIENT
+        System.out.println("Klient::::::::::::::::::::::::");
+        try (Connection con = DriverManager.getConnection(url, user, password);
+             PreparedStatement pst = con.prepareStatement("SELECT * FROM KLIENT");
+             ResultSet rs = pst.executeQuery()) {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columnsNumber; i++) {
+                    if (i > 1) System.out.print(",  ");
+                    String columnValue = rs.getString(i);
+                    System.out.print(rsmd.getColumnName(i) + ": " + columnValue);
+                }
+                System.out.println("");
+            }
+
+        } catch (SQLException ex) {
+            Logger lgr = Logger.getLogger(DBController.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+        System.out.print("-------------------------------------");
+        //////////////////////////ZAPYTANIE O KLIENT
+
     }
 }
