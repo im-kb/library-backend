3f0d9514807810e0105ad2629e54b75d9519796d
Marshall <kamil.budzik98@gmail.com>
2020-05-20 14:43:20 +0200

    SEPERATOR byl niepotrzebny

--- src/main/java/com/demo/springboot/service/impl/DBCode.java
+++ src/main/java/com/demo/springboot/service/impl/DBCode.java
@@ -12,18 +12,18 @@ public class DBCode {
     private static String url = "jdbc:postgresql://rogue.db.elephantsql.com:5432/cargbzfv";
     private static String user = "cargbzfv";
     private static String password = "wjhaujNlFknUAm2GQ3jz6HOh9UNLYRB8";
-    private static String SEPERATOR = "/n";
-    private static final String SPLIT_CHAR = ",";
+    private static String QUERY_RESULT_ROW = "";
+    private static final String SPLIT_CHAR = ";;";
 
     public static ArrayList<Ksiazka> readKsiazki() { //wczytywywanie ksiazek
         ksiazkaList = new ArrayList<>();
-        String query = "SELECT k.id_ksiazki,k.tytul,\n" +
+        String queryGetBooksForClient = "SELECT k.id_ksiazki,k.tytul,\n" +
                 "a.imie || ' ' || a.nazwisko as AUTOR,\n" +
                 "w.nazwa || ' ' ||w.miasto as WYDAWNICTWO,\n" +
                 "k.temat,k.jezyk_ksiazki,k.rok_wydania,k.dostepnosc,k.opis\n" +
                 "from ksiazka k natural join autor a natural join wydawnictwo w";
         try (Connection con = DriverManager.getConnection(url, user, password);
-             PreparedStatement pst = con.prepareStatement(query);
+             PreparedStatement pst = con.prepareStatement(queryGetBooksForClient);
              ResultSet rs = pst.executeQuery()) {
             ResultSetMetaData rsmd = rs.getMetaData();
             int columnsNumber = rsmd.getColumnCount();
@@ -31,11 +31,10 @@ public class DBCode {
             while (rs.next()) {
                 for (int i = 1; i <= columnsNumber; i++) {
                     columnValue = columnValue + rs.getString(i);
-                    columnValue = columnValue + ",";
+                    columnValue = columnValue + ";;";
                 }
-                columnValue = columnValue + "\n";
-                SEPERATOR = columnValue;
-                String[] tab = SEPERATOR.split(SPLIT_CHAR);
+                QUERY_RESULT_ROW = columnValue;
+                String[] tab = QUERY_RESULT_ROW.split(SPLIT_CHAR);
                 Ksiazka ks = new Ksiazka(tab[0], tab[1], tab[2], tab[3], tab[4], tab[5], tab[6], tab[7], tab[8]);
                 ksiazkaList.add(ks);
                 columnValue = "";
